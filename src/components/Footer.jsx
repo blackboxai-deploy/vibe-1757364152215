6d2745124a860b72faa21d5050d740176f42e425
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-09 02:12:42 +0530

    Add src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="footer" id="contact">
+
+    <h1>Alex Smith</h1>
+    <p>Frontend Developer</p>
+
+    <div class="social-links">
+        <a href="#"><i class='bx bxl-linkedin'></i></a>
+        <a href="#"><i class='bx bxl-github'></i></a>
+        <a href="#"><i class='bx bxl-twitter'></i></a>
+      </div>
+
+  </footer>
+  )
+}
+
+export default Footer
